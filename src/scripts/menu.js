968243b2b13e359499a551dbc40ed97255965e4c
RICHARD WESLEY BARBOSA FREITAS <wesleyrichard30@gmail.com>
2023-01-11 13:30:13 -0300

    correções no menu

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,5 +1,6 @@
 const headerNavBackground = document.querySelector(".header-nav-background");
 const headerNav = document.querySelector(".header-nav");
+const container = document.querySelector(".container");
 
 const exibir = () => {
     headerNav.classList.toggle("active");
@@ -9,11 +10,14 @@ const exibir = () => {
 const fechar = () => {
     headerNav.style.animationName = "slideof";
     headerNav.style.animationDuration = "0.5s";
+    container.style.pointerEvents = "none";
     setTimeout(() => {
+        container.style.pointerEvents = "all";
         headerNav.classList.toggle("active");
         headerNavBackground.style.display = "none";
         headerNav.style.animationName = "slidein";
-    }, 300);
+        
+    }, 400);
 };
 
 export { exibir, fechar };
